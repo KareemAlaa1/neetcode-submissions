6bd78e2476edd4c340417e0ce22dbc549d203fcf
Kareem Abu Al Enein <96543783+KareemAlaa1@users.noreply.github.com>
2026-04-22 17:41:15 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch:s){
+            if(ch==')'){
+                if(st.empty() || st.top()!='(')return false;
+                st.pop();
+            }else if(ch=='}'){
+                if(st.empty() || st.top()!='{')return false;
+                st.pop();
+            }else if(ch==']'){
+                if(st.empty() || st.top()!='[')return false;
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        if(st.empty())return true;
+        return false;
+    }
+};
